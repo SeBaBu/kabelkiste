b4346364a9d12586d24c60028d4df0bb2581b015
Sebastian Burschel <sebastian@burschel.com>
2020-03-20 08:24:19 +0100

    Added Packages

--- Raspberry Pi/first_install.sh
+++ Raspberry Pi/first_install.sh
@@ -1,10 +1,14 @@
 apt-get update
 apt-get upgrade
 
-apt-get install python3 vim-nox tmux nano zsh git autossh mosh sshuttle fail2ban avahi-daemon ufw lynx
+apt-get install python3 vim-nox tmux nano zsh git autossh mosh sshuttle fail2ban avahi-daemon ufw lynx npm apticron tidy
 
 
 ufw allow ssh
 ufw enable
 
-pip3 install requests bs4
+pip3 install requests bs4 prospector black isort
+
+
+npm install -g jsonlint
+npm install -g fixjson
